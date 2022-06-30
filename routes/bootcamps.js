57c479b64b69df4c13b0ace2f126ce7e42da7aaf
tharinduwick <tharindu.wickramasinghe-ext@harver.com>
2022-06-30 18:30:11 +0530

    init

---
+++ routes/bootcamps.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+} = require("../controllers/bootcamps");
+const advancedResults = require("../middlewares/advancedResults");
+const Bootcamp = require("../models/Bootcamp");
+
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, ), getBootcamps)
+  .post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
+module.exports = router;
